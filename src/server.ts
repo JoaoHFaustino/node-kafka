501722d4ca834f67e4cf1a06325f9b1dab6dd81e
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-07-20 17:47:16 -0300

    chore: adding initial settings

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { json } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
