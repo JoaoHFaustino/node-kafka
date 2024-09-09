a8fe6bd9129f80bdf5c0eb47797712ab138b89cc
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-09-09 19:56:53 -0300

    refactor: improving the condition and better specifying the error description in the event of an error

--- src/data/usecases/orders/addOrder.ts
+++ src/data/usecases/orders/addOrder.ts
@@ -1,11 +1,13 @@
+import { EKafkaErrorCodes } from "@/domain/enums/kafkaErrorCodes";
 import { generateTransactionIdHelper } from "@/domain/helpers/generateTransactionIdHelper";
-import { AddOrderProtocol } from "@/domain/protocols/orders/addOrder.interface";
+import { KafkaErrorMessages } from "@/domain/models/kafkaErrorMessages";
+import { IAddOrder } from "@/domain/protocols/orders/addOrder.interface";
 import KafkaProducer from "@/infra/kafka/kafkaProducer";
 
 const producer = new KafkaProducer();
 
-export class AddOrder implements AddOrderProtocol {
-    async addOrder(params: AddOrderProtocol.Params): Promise<AddOrderProtocol.Result> {
+export class AddOrder implements IAddOrder {
+    async addOrder(params: IAddOrder.Params): Promise<IAddOrder.Result> {
         try {
             const { orderId, customerId, items, totalAmount, paymentMethod, status } = params;
             await producer.connect();
@@ -24,14 +26,16 @@ export class AddOrder implements AddOrderProtocol {
                 { key: params.orderId, value: JSON.stringify(message) }
             ]);
 
-            const success = result.some(record => record.errorCode === 0);
+            const errorCode = result[0].errorCode as EKafkaErrorCodes
             await producer.disconnect();
 
-            if (success) {
-                return { success: true, transactionId: message.transactionId, message: 'Message successfully sent' };
-            } else {
-                return { success: false, message: 'Message sending failed' };
+            if (errorCode !== EKafkaErrorCodes.NoError) {
+                const errorMessage = KafkaErrorMessages[errorCode] || 'Unknown error'
+                return { success: false, message: `Message sending failed ${errorMessage}` };
             }
+
+            return { success: true, transactionId: message.transactionId, message: 'Message successfully sent' };
+
         } catch (error: any) {
             return { success: false, message: `Error during adding order: ${error.message}` };
         }
